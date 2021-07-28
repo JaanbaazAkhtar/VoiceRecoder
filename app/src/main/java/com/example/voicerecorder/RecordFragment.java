3add74910ee2bd2bdc16b823aa0e1baddb4050d2
JaanbaazAkhtar <jaanbaazakhtar786@gmail.com>
2021-07-29 01:33:33 +0530

    initial front end updated

--- app/src/main/java/com/example/voicerecorder/RecordFragment.java
+++ app/src/main/java/com/example/voicerecorder/RecordFragment.java
@@ -2,23 +2,30 @@ package com.example.voicerecorder;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link RecordFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class RecordFragment extends Fragment {
+public class RecordFragment extends Fragment implements View.OnClickListener {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+
+    private NavController navController;
+    private ImageView listBtn;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -53,6 +60,8 @@ public class RecordFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
+
     }
 
     @Override
@@ -60,5 +69,24 @@ public class RecordFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_record, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @NonNull Bundle savedInstanceState){
+        super.onViewCreated(view, savedInstanceState);
+
+        navController = Navigation.findNavController(view);
+
+        listBtn = view.findViewById(R.id.record_list_btn);
+        listBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v){
+        switch(v.getId()){
+            case R.id.record_list_btn:
+                navController.navigate(R.id.action_recordFragment_to_audioListFragment);
+                break;
+        }
     }
 }
